8dc242cd551386e6464963f801d6e09e633dbf1a
Avin <avin.hurry@digital.education.gov.uk>
2020-04-28 15:44:03 +0100

    Added the respond to release_bike method test & made it pass

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,11 @@
 require 'docking_station'
-describe DockingStation do 
+describe DockingStation do
+
+  it "responds to the method release_bike" do
+    expect(subject).to respond_to :release_bike  
+  end
 end
+
+
+# Add a test to your spec file that expects DockingStation
+# instances to respond_to the method release_bike
