c6964e44c5ed1cfce3b11edad2768b1a02c3ef90
Kabir Qazi <kqazi2@ford.com>
2020-04-28 16:32:11 +0100

    Convert test to single line test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,4 @@
 require 'docking_station'
 describe DockingStation do
-
-  it "responds to the method release_bike" do
-    expect(subject).to respond_to :release_bike  
-  end
+  it {is_expected.to respond_to :release_bike}  
 end
-
-
-# Add a test to your spec file that expects DockingStation
-# instances to respond_to the method release_bike
