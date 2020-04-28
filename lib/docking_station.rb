2a60812fe2e3885d0f5b04817a95c6722874879f
Kabir Qazi <kqazi2@ford.com>
2020-04-28 21:26:08 +0100

    Code cleanup

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,12 +4,3 @@ class DockingStation
     Bike.new
   end
 end
-
-
-
-# class ClassB
-#   attr_reader :object # lets you call 'some_instance_of_classb.object'
-#   def initialize
-#     @object = ClassA.new
-#   end
-# end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,8 +8,3 @@ describe DockingStation do
     expect(bike.working?).to eq(true)
   end
 end
-
-# Add a test to your DockingStation specification that a) gets a bike,
-# and then b) expects the bike to be working
-# Make this test pass
-# Feature-test the feature again.
