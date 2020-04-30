f7d5a93ed0176be7ffc4b69edf21bdb667d80668
Kabir Qazi <kqazi2@ford.com>
2020-04-30 14:53:18 +0100

    Adding broken? test and method

--- lib/bike.rb
+++ lib/bike.rb
@@ -2,4 +2,8 @@ class Bike
   def working?
     true
   end
+
+  def broken?
+    true
+  end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,10 @@
 require 'bike'
 describe Bike do
   it {is_expected.to respond_to :working?}
+
+  it "report a broken bike" do
+    bike = Bike.new
+    expect(bike.broken?).to eq(true)
+end
+
 end
